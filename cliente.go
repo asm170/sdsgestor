404496e2747fb699ac4a0e9871e890a5041c197d
Jorge Garcia Gomez <jgg107@alu.ua.es>
2017-04-13 21:39:41 +0200

    Completada la tarea de login en cliente.

--- cliente.go
+++ cliente.go
@@ -44,15 +44,12 @@ func limpiarPantallaWindows() {
 	cmd.Run()
 }
 
-func loginUsuario(usuario string, password string, validado bool) bool {
-	return validado
-}
-
 func main() {
 	var opMenuPrincipal string
 	var opMenuUsuario string
 	var usuario string
-	var password string
+	var passLogin string
+	//var passAES string
 	var passRegistro string
 	var repitePassRegistro string
 	var mensajeMenuPrincipal string
@@ -95,63 +92,80 @@ func main() {
 				usuario = scanner.Text()
 				fmt.Print("Password: ")
 				scanner.Scan()
-				password = scanner.Text()
-				//fmt.Printf("[LOG] [Login] Usuario: [%s] Password: [%s]\n", usuario, password)
+				passLogin = scanner.Text()
 				// Comprobamos que el usuario es correcto
-				if (len(usuario) > 0 && len(password) > 0) && loginUsuario(usuario, password, true) {
-					mensajeErrorLogin = ""
-					limpiarPantallaWindows()
-					opMenuUsuario = "0"
-					for opMenuUsuario != "5" {
-						if opMenuUsuario == "0" {
-							// Mostramos el menu del usuario
-							fmt.Println("+---------------------------+")
-							fmt.Println("|  Panel de administracion  |")
-							fmt.Println("+---------------------------+")
-							fmt.Printf(mensajeAdministracion)
-							fmt.Printf("Estas logueado como: [%s] \n", usuario)
-							fmt.Println("[1] Buscar una cuenta")
-							fmt.Println("[2] Añadir una nueva cuenta")
-							fmt.Println("[3] Modificar una cuenta")
-							fmt.Println("[4] Eliminar una cuenta")
-							fmt.Println("[5] Salir")
-							fmt.Print("Elige una opcion: ")
-							scanner.Scan()
-							opMenuUsuario = scanner.Text()
-						}
-						if opMenuUsuario != "5" {
-							limpiarPantallaWindows()
-							switch opMenuUsuario {
-							case "1":
-								fmt.Println("Buscar una cuenta")
-								mensajeAdministracion = ""
-							case "2":
-								fmt.Println("Añadir una nueva cuenta")
-								mensajeAdministracion = ""
-							case "3":
-								fmt.Println("Modificar una cuenta")
-								mensajeAdministracion = ""
-							case "4":
-								fmt.Println("Eliminar una cuenta")
-								mensajeAdministracion = ""
+				if len(usuario) > 0 && len(passLogin) > 0 {
+					// Resumimos el password con SHA3
+					passLoginSHA3 := hashSha512(passLogin)
+					// Partimos el SHA3 generado
+					passSHA3 := passLoginSHA3[0:32]
+					//passAES = encode64(passLoginSHA3[32:64])
+					//fmt.Printf("[DEBUG]	usuario	[%s]	password	[%s] \n", usuario, encode64(passSHA3))
+					// Convertimos a JSON los datos que le enviaremos al servidor
+					re := jsonIdentificacion{Usuario: usuario, Password: encode64(passSHA3)}
+					jsonIdentificacion, err := json.Marshal(&re)
+					chk(err)
+					// Enviamos al servidor los datos de registro mediante POST
+					r, err := client.Post("https://localhost:10441/login", "application/json", bytes.NewBuffer(jsonIdentificacion))
+					chk(err)
+					// Recogemos la respuesta del servidor y lo convertimos a JSON
+					decoder := json.NewDecoder(r.Body)
+					var jis jsonIdentificacionServidor
+					decoder.Decode(&jis)
+					/*
+						Comprobamos que los datos de usuario
+						son correctos, si son correctos, mostramos
+						el menu del usuario. Si no son correctos
+						volvemos a pedir al usuario los datos
+					*/
+					if jis.Valido == true {
+						mensajeErrorLogin = ""
+						limpiarPantallaWindows()
+						opMenuUsuario = "0"
+						for opMenuUsuario != "5" {
+							if opMenuUsuario == "0" {
+								// Mostramos el menu del usuario
+								fmt.Println("+---------------------------+")
+								fmt.Println("|  Panel de administracion  |")
+								fmt.Println("+---------------------------+")
+								fmt.Printf(mensajeAdministracion)
+								fmt.Printf("Estas logueado como: [%s] \n", usuario)
+								fmt.Println("[1] Buscar una cuenta")
+								fmt.Println("[2] Añadir una nueva cuenta")
+								fmt.Println("[3] Modificar una cuenta")
+								fmt.Println("[4] Eliminar una cuenta")
+								fmt.Println("[5] Salir")
+								fmt.Print("Elige una opcion: ")
+								scanner.Scan()
+								opMenuUsuario = scanner.Text()
 							}
-							// Colocamos el flag a 0 para que vuelva a mostrar el menu del usuario
-							opMenuUsuario = "0"
+							if opMenuUsuario != "5" {
+								limpiarPantallaWindows()
+								switch opMenuUsuario {
+								case "1":
+									fmt.Println("Buscar una cuenta")
+									mensajeAdministracion = ""
+								case "2":
+									fmt.Println("Añadir una nueva cuenta")
+									mensajeAdministracion = ""
+								case "3":
+									fmt.Println("Modificar una cuenta")
+									mensajeAdministracion = ""
+								case "4":
+									fmt.Println("Eliminar una cuenta")
+									mensajeAdministracion = ""
+								}
+								// Colocamos el flag a 0 para que vuelva a mostrar el menu del usuario
+								opMenuUsuario = "0"
+							}
 						}
+						// Colocamos el flag a 0 para que vuelva a mostrar el menu principal
+						opMenuPrincipal = "0"
+						limpiarPantallaWindows()
+					} else {
+						mensajeErrorLogin = "[ERROR] " + jis.Mensaje + "\n"
 					}
-					// Colocamos el flag a 0 para que vuelva a mostrar el menu principal
-					opMenuPrincipal = "0"
-					limpiarPantallaWindows()
-				} else {
-					mensajeErrorLogin = "[ERROR] El nombre de usuario y/o password son incorrectos\n"
 				}
-				/*
-					TODO: Comprobamos que los datos de usuario
-					son correctos, si son correctos, mostramos
-					el menu del usuario. Si no son correctos
-					volvemos a pedir al usuario los datos un
-					determinado numero de veces
-				*/
 			case "2": // Registro de usuario
 				limpiarPantallaWindows()
 				fmt.Println("+---------------------------------------------------+")
@@ -167,7 +181,13 @@ func main() {
 				fmt.Print("Repite el password: ")
 				scanner.Scan()
 				repitePassRegistro = scanner.Text()
-				// Comprobamos que el usuario ha introducido todos los datos
+				/*
+					Se comprueba que los datos proporcionados
+					por el usuario son correctos, si no son correctos
+					se mostraria al usuario los errores. Si son correctos
+					se envian al servidor para añadir el nuevo usuario
+					en el archivo JSON
+				*/
 				if len(usuario) > 0 && len(passRegistro) > 0 && len(repitePassRegistro) > 0 {
 					if passRegistro == repitePassRegistro {
 						mensajeErrorRegistro = ""
@@ -204,16 +224,7 @@ func main() {
 					mensajeErrorRegistro = "[ERROR] Te has dejado campos sin rellenar\n"
 					opMenuPrincipal = "2" // Mostramos el registro otra vez
 				}
-				/*
-					TODO: Se comprueba que los datos proporcionados
-					por el usuario son correctos, si no son correctos
-					se mostraria al usuario los errores. Si son correctos
-					se envian al servidor para añadir el nuevo usuario
-					en el archivo JSON
-				*/
 			}
-
 		}
 	}
-
 }
